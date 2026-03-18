bba4a256cf70e3edf2ede1d447607f0db240b67c
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-03-18 20:18:02 +0530

    updated library functions

--- vars/owaspScan.groovy
+++ vars/owaspScan.groovy
@@ -2,10 +2,12 @@ def call() {
 
     sh 'mkdir -p reports'
 
+    def dcHome = tool 'dependency-check'
+
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
 
-        sh '''
-        /usr/bin/dependency-check.sh \
+        sh """
+        ${dcHome}/bin/dependency-check.sh \
             --project solar-system \
             --scan . \
             --format XML \
@@ -13,7 +15,7 @@ def call() {
             --out reports \
             --failOnCVSS 8 \
             --nvdApiKey $NVD_API_KEY
-        '''
+        """
     }
 
     dependencyCheckPublisher pattern: 'reports/dependency-check-report.xml'
