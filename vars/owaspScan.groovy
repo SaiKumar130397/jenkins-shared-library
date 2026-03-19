beb705943a8c1d52cb3cc3710430dc3996fc54c4
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-03-19 12:16:34 +0530

    updated owaspscan function

--- vars/owaspScan.groovy
+++ vars/owaspScan.groovy
@@ -7,19 +7,25 @@ def call() {
 
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
 
-        sh """
-        ${dcHome}/bin/dependency-check.sh \
-            --project solar-system \
-            --scan . \
-            --format XML \
-            --format HTML \
-            --out reports \
-            --data /var/lib/jenkins/odc-data \
-            --nvdApiKey $NVD_API_KEY \
-            --nvdApiDelay 2000
-            --noupdate
-            || true
-        """
+        def status = sh(
+            script: """
+            ${dcHome}/bin/dependency-check.sh \
+                --project solar-system \
+                --scan . \
+                --format XML \
+                --format HTML \
+                --out reports \
+                --data /var/lib/jenkins/odc-data \
+                --nvdApiKey $NVD_API_KEY \
+                --nvdApiDelay 2000 \
+                --noupdate \
+                --disableYarnAudit \
+                --disableOssIndex
+            """,
+            returnStatus: true
+        )
+
+        echo "OWASP scan exit code: ${status}"
     }
 
     dependencyCheckPublisher pattern: 'reports/dependency-check-report.xml'
