1f8bcd0b0eab6602cafc375b8a463c2e61bc3d3d
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-03-19 20:04:57 +0530

    updated deployEKS function

--- vars/deployEKS.groovy
+++ vars/deployEKS.groovy
@@ -3,8 +3,9 @@ def call(String cluster, String region, String image, String env) {
     sh """
     aws eks update-kubeconfig --region ${region} --name ${cluster}
 
-    sed -i 's|IMAGE_PLACEHOLDER|${image}|g' k8s/${env}/deployment.yaml
-
-    kubectl apply -f k8s/${env}/
+    helm upgrade --install solar-${env} ./helm/solar-system \
+        --set image.repository=${image.split(':')[0]} \
+        --set image.tag=${image.split(':')[1]} \
+        -f helm/solar-system/values-${env}.yaml
     """
 }
